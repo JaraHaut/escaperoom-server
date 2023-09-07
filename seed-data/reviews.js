bdcc2f00726820d2eb3c31d8807b7d6d2c1a7ac0
Jara Haut <jara.haut@gmail.com>
2023-09-07 03:53:41 +0100

    update reviews seeds

--- seed-data/reviews.js
+++ seed-data/reviews.js
@@ -180,4 +180,185 @@ module.exports = [
     picture:
       "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap8.jpeg?alt=media&token=fa1cd2fd-cea2-42b9-8c26-b0f9e39e83f8",
   },
+  {
+    id: 13,
+    condition: 4,
+    property_id: 12,
+    confort: 3,
+    safety: 5,
+    management: 1,
+    rating: 3.25,
+    comments:
+      "Beautiful and nice location, the flat has some mold issues that are difficult to repair. The letting agent takes a lot of time to fix or repair anything in the flat and when it does, always the cheapest useless solution. It is a really small overpriced flat, old and not very well maintained. During the 2 years we rented this flat they didn't raise the price, which is nice, but they tried to take advantage of our deposit and we had to complain with photos and evidence to prove we didn't damage anything. We won't repeat with the same letting agent.",
+    price: 1780,
+    date: 2021,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap13.jpeg?alt=media&token=bf1ef2fb-dadb-45c6-81ba-a23462614d27",
+  },
+  {
+    id: 14,
+    condition: 4,
+    property_id: 11,
+    confort: 5,
+    safety: 4,
+    management: 5,
+    rating: 4.5,
+    comments:
+      "Furniture was well-maintained and comfortable. Excellent thermal insulation and noise reduction. Management was top-notch, addressing concerns promptly. The area is safe and has excellent connectivity. They asked to raise the price more than 20% after the first year so we decided to leave.",
+    price: 2340,
+    date: 2019,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap16.jpeg?alt=media&token=78e9c888-093f-4837-a8fc-0b7e5d0d8219",
+  },
+  {
+    id: 15,
+    condition: 1,
+    property_id: 10,
+    confort: 1,
+    safety: 3,
+    management: 1,
+    rating: 1.5,
+    comments:
+      "The furniture was in poor condition, needing immediate attention. Comfort was compromised due to poor thermal insulation and noise issues. Management was lacking; maintenance requests were often ignored. Safety and connectivity were average. The letting agent tried to scam us with the deposit, we got it back after 3 months.",
+    price: 1320,
+    date: 2022,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap15.jpeg?alt=media&token=f48eb5cf-0785-410b-b7ae-02ca223d62c3",
+  },
+  {
+    id: 16,
+    condition: 4,
+    property_id: 9,
+    confort: 4,
+    safety: 4,
+    management: 4,
+    rating: 4.0,
+    comments:
+      "The property was unfurnished but the finishes were in great condition. The property had excellent thermal insulation and noise reduction. The heating failed a couple of times in winter but itwas fixed quickly. The landlord's management was attentive and quick to address any issues. The area is safe and offers great connectivity.",
+    price: 1970,
+    date: 2022,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap12.jpeg?alt=media&token=65daf687-55aa-49b5-8342-056221d847ef",
+  },
+  {
+    id: 17,
+    condition: 4,
+    property_id: 8,
+    confort: 3,
+    safety: 2,
+    management: 5,
+    rating: 3.5,
+    comments:
+      "Furniture was well-kept and added to the overall comfort. Thermal insulation was good, and noise insulation was satisfactory despite the near location of a tube station. There was a leak from the flat upstairs that damaged our ceiling but it was solved. The landlord's management was excellent. The area is not very safe, I was scared to walk alone at night.",
+    price: 2530,
+    date: 2020,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap3.jpeg?alt=media&token=fc21760a-54a3-4261-962b-8fba9c31fe27",
+  },
+  {
+    id: 18,
+    condition: 5,
+    property_id: 7,
+    confort: 5,
+    safety: 4,
+    management: 5,
+    rating: 4.75,
+    comments:
+      "Everything felt new and well-maintained. The thermal insulation kept the place cozy, and noise insulation was impressive. The furniture was in excellent condition. The landlord's management was great, always prompt in addressing issues. The area is safe and well-connected. The property was sold so we they terminated our contract early.",
+    price: 3600,
+    date: 2017,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap5.jpeg?alt=media&token=f031b3b0-c2de-4501-b3f5-c0fc1a513017",
+  },
+  {
+    id: 19,
+    condition: 3,
+    property_id: 6,
+    confort: 2,
+    safety: 3,
+    management: 3,
+    rating: 2.75,
+    comments:
+      "The furniture needed some updates, but overall, it was acceptable. The comfort level was good, especially the heating. Noise insulation could be better. Management was average; responses were delayed at times. The area feels safe but lacks good connectivity. They never raised the rent in 3 years, there was a good relationship between us, the tenants, and the landlord.",
+    price: 1120,
+    date: 2023,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap14.jpeg?alt=media&token=939f4955-ab1f-40d4-8f61-cb6902da032a",
+  },
+  {
+    id: 20,
+    condition: 3,
+    property_id: 5,
+    confort: 2,
+    safety: 4,
+    management: 3,
+    rating: 3.0,
+    comments:
+      "Furniture was a mix of old and new. Comfort was decent, but noise insulation was lacking. Single glazing only and old sash windows. Management was responsive, but there were some maintenance delays. The area is safe, with good connectivity to amenities. Only buses in the area, no tube.",
+
+    price: 1360,
+    date: 2018,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap10.jpeg?alt=media&token=62fd2c47-456e-4c65-b0f1-43d6b4cda8a2",
+  },
+  {
+    id: 21,
+    condition: 3,
+    property_id: 4,
+    confort: 2,
+    safety: 4,
+    management: 3,
+    rating: 3,
+    comments:
+      "Thermal insulation was decent, but noise insulation could be better. The furniture needed some improvements, but it was comfortable. Management was average; responses were slow. The area is safe, and connectivity is convenient.",
+    price: 1580,
+    date: 2023,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap9.jpeg?alt=media&token=5889e29b-613e-4b6c-9f65-9a645a2fb4d4",
+  },
+  {
+    id: 22,
+    condition: 3,
+    property_id: 3,
+    confort: 4,
+    safety: 4,
+    management: 3,
+    rating: 3.5,
+    comments:
+      "Furniture was in good condition and added to the comfort. The property had excellent thermal insulation and noise reduction. Management was efficient and responsive. The area is safe but very far away from amenities and transport links.",
+    price: 1360,
+    date: 2018,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap4.jpeg?alt=media&token=e9e588e9-7cbc-410d-aa0f-4142baf2b978",
+  },
+  {
+    id: 23,
+    condition: 3,
+    property_id: 2,
+    confort: 4,
+    safety: 4,
+    management: 3,
+    rating: 3.5,
+    comments:
+      "The comfort level was good, especially the heating. Noise insulation could be better. The furniture needed some updates, but overall, it was acceptable. Management was average; responses were delayed at times. The area feels safe but lacks good connectivity. They never raised the rent in 3 years, there was a good relationship between us, the tenants, and the landlord.",
+    price: 1360,
+    date: 2018,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap11.jpeg?alt=media&token=a6e9d709-a49a-4d57-bba8-5d661f577aa4",
+  },
+  {
+    id: 24,
+    condition: 3,
+    property_id: 1,
+    confort: 4,
+    safety: 4,
+    management: 3,
+    rating: 3.5,
+    comments:
+      "The furniture was in excellent condition. Everything felt new and well-maintained. The thermal insulation kept the place cozy, and noise insulation was impressive. The landlord's management was great, always prompt in addressing issues. The area is safe and well-connected. The property was sold so we they terminated our contract early.",
+    price: 6580,
+    date: 2019,
+    picture:
+      "https://firebasestorage.googleapis.com/v0/b/escaperoom-de5f1.appspot.com/o/cheap8.jpeg?alt=media&token=fa1cd2fd-cea2-42b9-8c26-b0f9e39e83f8",
+  },
 ];
